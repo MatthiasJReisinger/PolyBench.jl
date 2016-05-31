f32148b7e997c7fc6dc1e6f4f407f677a3ed0d12
Matthias Reisinger <matthias.j.reisinger@gmail.com>
2016-05-31 22:02:07 +0200

    cleanup

--- benchmarks/sum3d.jl
+++ benchmarks/sum3d.jl
@@ -1,3 +1,5 @@
+using BenchmarkTools
+
 @polly function sum3d!(A,B)
     m,n,o = size(A)
     @inbounds for i=1:m, j=1:m, k=1:m
@@ -5,17 +7,11 @@
     end
 end
 
-N = 1024
 M = 1024
+N = 1024
 O = 1024
 
 A = zeros(Float32,M,N,O)
 B = zeros(Float32,M,N)
-time_start = time()
-#for i=1:10
-    sum3d!(A,B)
-#end
-time_end = time()
 
-time_overall = time_end - time_start
-println("Time: ", time_overall)
+println(time(@benchmark sum3d!(A,B) samples=1 evals=5))
