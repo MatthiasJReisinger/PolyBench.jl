93c775bab0be7f93a4c56516744c32d65df3a179
Matthias Reisinger <matthias.j.reisinger@gmail.com>
2016-06-17 19:07:39 +0200

    Added PolyBench stencil benchmarks

--- src/PolyBench.jl
+++ src/PolyBench.jl
@@ -529,4 +529,134 @@ end
     end
 end
 
+############
+# stencils #
+############
+
+@polly function kernel_adi(tsteps, u, v, p, q)
+    n = size(u,1)
+
+    DX = 1.0 / n
+    DY = 1.0 / n
+    DT = 1.0 / tsteps
+    B1 = 2.0
+    B2 = 1.0
+    mul1 = B1 * DT / (DX * DX)
+    mul2 = B2 * DT / (DY * DY)
+
+    a = -mul1 /  2.0;
+    b = 1.0 + mul1
+    c = a
+    d = -mul2 / 2.0
+    _e = 1.0 + mul2
+    f = d
+
+    for t = 1:tsteps
+        # Column Sweep
+        for i = 2:(n-1)
+            v[1,i] = 1.0
+            p[i,1] = 0.0
+            q[i,1] = v[1,i]
+            for j = 2:(n-1)
+                p[i,j] = -c / (a * p[i,j-1] + b)
+                q[i,j] = (-d*u[j,i-1]+(1.0+2.0*d)*u[j,i] - f*u[j,i+1]-a*q[i,j-1])/(a*p[i,j-1]+b)
+            end
+
+            v[n,i] = 1.0
+            for j = (n-1):-1:2
+                v[j,i] = p[i,j] * v[j+1,i] + q[i,j]
+            end
+        end
+
+        # Row Sweep
+        for i = 2:(n-1)
+            u[i,1] = 1.0
+            p[i,1] = 0.0
+            q[i,1] = u[i,0]
+            for j = 2:(n-1)
+                p[i,j] = -f / (d*p[i,j-1] + _e)
+                q[i,j] = (-a*v[i-1,j]+(1.0+2.0*a)*v[i,j] - c*v[i+1,j]-d*q[i,j-1])/(d*p[i,j-1]+_e)
+            end
+
+            u[i,n] = 1.0
+            for j = (n-1):-1:2
+                u[i,j] = p[i,j] * u[i,j+1] + q[i,j]
+            end
+        end
+    end
+end
+
+@polly function kernel_fdtd_2d(ex, ey, hz, _fict_)
+    nx,ny = size(ex)
+    tmax = size(_fict_,1)
+
+    for t = 1:tmax
+        for j = 1:ny
+            ey[1,j] = _fict_[t]
+        end
+        for i = 2:nx, j = 1:ny
+            ey[i,j] = ey[i,j] - 0.5 * (hz[i,j] - hz[i-1,j])
+        end
+        for i = 1:nx, j = 2:ny
+            ex[i,j] = ex[i,j] - 0.5 * (hz[i,j] - hz[i,j-1])
+        end
+        for i = 1:(nx - 1), j = 1:(ny - 1)
+            hz[i,j] = hz[i,j] - 0.7 * (ex[i,j+1] - ex[i,j] + ey[i+1,j] - ey[i,j])
+        end
+    end
+end
+
+@polly function kernel_heat_3d(tsteps, A, B)
+    n = size(A,1)
+
+    for t = 1:tsteps
+        for i = 2:(n-1), j = 2:(n-1), k = 2:(n-1)
+            B[i,j,k] =   0.125 * (A[i+1,j,k] - 2.0 * A[i,j,k] + A[i-1,j,k]) +
+                         0.125 * (A[i,j+1,k] - 2.0 * A[i,j,k] + A[i,j-1,k]) +
+                         0.125 * (A[i,j,k+1] - 2.0 * A[i,j,k] + A[i,j,k-1]) +
+                         A[i,j,k]
+        end
+        for i = 2:(n-1), j = 2:(n-1), k = 2:(n-1)
+            A[i,j,k] =   0.125 * (B[i+1,j,k] - 2.0 * B[i,j,k] + B[i-1,j,k]) +
+                         0.125 * (B[i,j+1,k] - 2.0 * B[i,j,k] + B[i,j-1,k]) +
+                         0.125 * (B[i,j,k+1] - 2.0 * B[i,j,k] + B[i,j,k-1]) +
+                         B[i,j,k]
+        end
+    end
+end
+
+@polly function kernel_jacobi_1d(tsteps, A, B)
+    n = size(A,1)
+
+    for t = 1:tsteps
+        for i = 2:(n-1)
+            B[i] = 0.33333 * (A[i-1] + A[i] + A[i + 1])
+        end
+        for i = 2:(n-1)
+            A[i] = 0.33333 * (B[i-1] + B[i] + B[i + 1])
+        end
+    end
+end
+
+@polly function kernel_jacobi_2d(tsteps, A, B)
+    n = size(A,1)
+
+    for t = 1:tsteps
+        for i = 2:(n-1), j = 2:(n-1)
+            B[i,j] = 0.2 * (A[i,j] + A[i,j-1] + A[i,1+j] + A[1+i,j] + A[i-1,j])
+        end
+        for i = 2:(n-1), j = 2:(n-1)
+            A[i,j] = 0.2 * (B[i,j] + B[i,j-1] + B[i,1+j] + B[1+i,j] + B[i-1,j])
+        end
+    end
+end
+
+@polly function kernel_seidel_2d(tsteps, A)
+    n = size(A,1)
+
+    for t = 1:tsteps, i = 2:(n-2), j = 2:(n-2)
+        A[i,j] = (A[i-1,j-1] + A[i-1,j] + A[i-1,j+1] + A[i,j-1] + A[i,j] + A[i,j+1] + A[i+1,j-1] + A[i+1,j] + A[i+1,j+1])/9.0
+    end
+end
+
 end # module
