c3a4901d7b210bb8239e609e76e99869d0ba15ee
Matthias Reisinger <matthias.j.reisinger@gmail.com>
2016-06-21 16:24:37 +0200

    Remote Clean up micro benchmarks

--- src/MicroBenchmarks.jl
+++ src/MicroBenchmarks.jl
@@ -3,99 +3,76 @@ module MicroBenchmarks
 @polly function gemm!(A,B,C)
     m,n = size(A)
     n,o = size(B)
-    @inbounds for i=1:m, j=1:o, k=1:n
+    for i=1:m, j=1:o, k=1:n
         C[i,j] += A[i,k] * B[k,j]
     end
 end
 
 @polly function sqmm!(A,B,C)
     n,n = size(A)
-    @inbounds for i=1:n, j=1:n, k=1:n
+    for i=1:n, j=1:n, k=1:n
         C[i,j] += A[i,k] * B[k,j]
     end
 end
 
-@polly function row_sum!(A,B)
-    m,n = size(B)
-    @inbounds for i=1:m, j=1:n
-        A[i] += B[i,j]
-    end
-end
-
-@polly function sum3d(A)
-    a = zero(eltype(A))
-    m,n,o = size(A)
-    @inbounds for k=1:o, j=1:n, i=1:m
-        a += A[i,j,k]
-    end
-    return a
-end
-
-@polly function sum3d_cube(A)
-    a = zero(eltype(A))
-    m,n,o = size(A)
-    @inbounds for k=1:m, j=1:m, i=1:m
-        a += A[i,j,k]
-    end
-    return a
-end
-
-@polly function sum2d(A)
-    a = zero(eltype(A))
-    m,n = size(A)
-    @inbounds for i=1:m, j=1:n
-        a += A[i,j]
-    end
-    return a
-end
-
-@polly function sum2d_square(A)
-    a = zero(eltype(A))
-    m,n = size(A)
-    @inbounds for i=1:m, j=1:m
-        a += A[i,j]
-    end
-    return a
-end
-
 @polly function copy3d!(A,B)
     m,n,o = size(A)
-    @inbounds for i=1:m, j=1:n, k=1:o
+    for i=1:m, j=1:n, k=1:o
         A[i,j,k] = B[i,j,k]
     end
 end
 
 @polly function copy3d_cube!(A,B)
     m,n,o = size(A)
-    @inbounds for i=1:m, j=1:m, k=1:m
+    for i=1:m, j=1:m, k=1:m
         A[i,j,k] = B[i,j,k]
     end
 end
 
 @polly function copy2d!(A,B)
     m,n = size(A)
-    @inbounds for i=1:m, j=1:n
+    for i=1:m, j=1:n
         A[i,j] = B[i,j]
+    end
+end
+
+@polly function copy2d_triangular!(A,B::UpperTriangular)
+    m,n = size(A)
+    for i=1:m, j=1:n
+        A[i,j] = B.data[i,j]
     end
 end
 
 @polly function copy2d_square!(A,B)
     m,n = size(A)
-    @inbounds for i=1:m, j=1:m
+    for i=1:m, j=1:m
         A[i,j] = B[i,j]
     end
 end
 
-@polly function vector_sum!(A,B)
-    n = size(A,1)
-    @inbounds for i = 1:n
-        B[0] += A[i]
+@polly function for_step_2_up(A)
+    for i = 1:2:1024
+        A[i] = 0
     end
 end
 
-@polly function vector_sum2!(A,B)
-    @inbounds for x in A
-        B[0] += x
+@polly function for_step_parametric(A,s)
+    n = size(A,1)
+    for i = 1:s:1024
+        A[i] = 0
+    end
+end
+
+@polly function for_step_1_down(A)
+    n = size(A,1)
+    for i = 1024:-1:1
+        A[i] = 0
+    end
+end
+
+@polly function for_step_2_nested(A)
+    for i = 1:1024, j = 1:2:1024
+        A[i,j] = 0
     end
 end
 
