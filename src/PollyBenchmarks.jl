5ca9cf02ed7e023c8849c5f2ed2102e9badf59c1
Matthias Reisinger <matthias.j.reisinger@gmail.com>
2016-06-21 17:01:54 +0200

    Clean up and enable more accurate time profiling

--- src/PollyBenchmarks.jl
+++ src/PollyBenchmarks.jl
@@ -3,16 +3,18 @@ module PollyBenchmarks
 include("MicroBenchmarks.jl")
 include("PolyBench.jl")
 
+# Prints the time needed to execute `ex` in microseconds.
 macro benchmark(ex, evals)
     quote
-#        $(esc(ex)) # force compilation
-        start_time = time()
+        $(esc(ex)) # force compilation
+        total_time = 0
         for i = 1:$evals
+            start_time = time_ns()
             $(esc(ex))
+            end_time = time_ns()
+            total_time += (end_time - start_time)
         end
-        end_time = time()
-        total_time = end_time - start_time
-        println(total_time)
+        println(round(Int, total_time / 10^3 / $evals))
     end
 end
 
@@ -23,14 +25,14 @@ function run()
     big_dim = 1024
     small_dim = 256
 
-    big1d1 = zeros(Float32,1024)
-    big1d2 = zeros(Float32,1024)
-    big1d3 = zeros(Float32,1024)
-    big1d4 = zeros(Float32,1024)
-    big1d5 = zeros(Float32,1024)
-    big1d6 = zeros(Float32,1024)
-    big1d7 = zeros(Float32,1024)
-    big1d8 = zeros(Float32,1024)
+    big1d1 = zeros(Float32,big_dim)
+    big1d2 = zeros(Float32,big_dim)
+    big1d3 = zeros(Float32,big_dim)
+    big1d4 = zeros(Float32,big_dim)
+    big1d5 = zeros(Float32,big_dim)
+    big1d6 = zeros(Float32,big_dim)
+    big1d7 = zeros(Float32,big_dim)
+    big1d8 = zeros(Float32,big_dim)
     big2d1 = zeros(Float32,big_dim,big_dim)
     big2d2 = zeros(Float32,big_dim,big_dim)
     big2d3 = zeros(Float32,big_dim,big_dim)
@@ -45,21 +47,22 @@ function run()
     small3d1 = zeros(Float32,small_dim,small_dim,small_dim)
     small3d2 = zeros(Float32,small_dim,small_dim,small_dim)
 
+    upperTriangular = UpperTriangular(zeros(Float32,big_dim,big_dim))
+
     # Miscellaneous micro benchmarks
 
     @benchmark(MicroBenchmarks.gemm!(big2d1,big2d2,big2d3),3)
     @benchmark(MicroBenchmarks.sqmm!(big2d1,big2d2,big2d3),3)
-    @benchmark(MicroBenchmarks.row_sum!(big2d1,big2d2),1000)
-    @benchmark(MicroBenchmarks.sum3d(big3d),1)
-    @benchmark(MicroBenchmarks.sum3d_cube(big3d),1)
-    @benchmark(MicroBenchmarks.sum2d(big2d1),100)
-    @benchmark(MicroBenchmarks.sum2d_square(big2d1),100)
     @benchmark(MicroBenchmarks.copy3d!(small3d1,small3d2),1)
     @benchmark(MicroBenchmarks.copy3d_cube!(small3d1,small3d2),1)
     @benchmark(MicroBenchmarks.copy2d!(big2d1,big2d2),1000)
+    @benchmark(MicroBenchmarks.copy2d!(big2d1,upperTriangular),1000)
+    @benchmark(MicroBenchmarks.copy2d_triangular!(big2d1,upperTriangular),1000)
     @benchmark(MicroBenchmarks.copy2d_square!(big2d1,big2d2),1000)
-    @benchmark(MicroBenchmarks.vector_sum!(big1d1,big1d1),100000)
-    @benchmark(MicroBenchmarks.vector_sum2!(big1d1,big1d1),100000)
+    @benchmark(MicroBenchmarks.for_step_2_up(big1d1), 1)
+    @benchmark(MicroBenchmarks.for_step_parametric(big1d1,1), 1)
+    @benchmark(MicroBenchmarks.for_step_1_down(big1d1), 1)
+    @benchmark(MicroBenchmarks.for_step_2_nested(big2d1), 1)
 
     # PolyBench datamining
 
@@ -70,10 +73,10 @@ function run()
 
     @benchmark(PolyBench.kernel_2mm(1.0,1.0,big2d1,big2d2,big2d3,big2d4,big2d5),1)
     @benchmark(PolyBench.kernel_3mm(big2d1,big2d2,big2d3,big2d4,big2d5,big2d6,big2d7),1)
-    @benchmark(PolyBench.kernel_atax(big2d1,big1d1,big1d2,big1d3),1000)
-    @benchmark(PolyBench.kernel_bicg(big2d1,big1d1,big1d2,big1d3,big1d4),1000)
+    @benchmark(PolyBench.kernel_atax(big2d1,big1d1,big1d2,big1d3),1)
+    @benchmark(PolyBench.kernel_bicg(big2d1,big1d1,big1d2,big1d3,big1d4),1)
     @benchmark(PolyBench.kernel_doitgen(small3d1,small2d1,small1d1),1)
-    @benchmark(PolyBench.kernel_mvt(big1d1,big1d2,big1d3,big1d4,big2d1),1000)
+    @benchmark(PolyBench.kernel_mvt(big1d1,big1d2,big1d3,big1d4,big2d1),1)
 
     # PolyBench linear-algebra/blas
 
