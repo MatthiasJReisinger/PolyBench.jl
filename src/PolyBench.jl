c860de3fc418e5f2bc2727bcdc9ec48781e22fc1
Hrishikesh Vaidya <hrishikeshv95@gmail.com>
2016-10-23 21:57:35 +0530

    Polybench.jl modified

--- src/PolyBench.jl
+++ src/PolyBench.jl
@@ -4,6 +4,7 @@ using BenchmarkTools
 
 const SUITE = BenchmarkGroup()
 const BENCH_DIRS = ["datamining",
+					"polyNN/CNN",
                     "linear-algebra/blas",
                     "linear-algebra/kernels",
                     "linear-algebra/solvers",
@@ -12,6 +13,7 @@ const BENCH_DIRS = ["datamining",
 
 function load!()
     for bench_dir in BENCH_DIRS
+		print("loading $bench_dir...")
         bench_dir_path = joinpath(dirname(@__FILE__), bench_dir)
         for bench_file in filter(x -> endswith(x, "jl"), readdir(bench_dir_path))
             bench_file_path = joinpath(bench_dir_path, bench_file)
